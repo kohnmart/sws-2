9a9a55fc89b47ff05f8d01757659e62832ac2863
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-05-18 18:52:12 +0200

    Update menuApi.ts

--- popup-menu/menuApi.ts
+++ popup-menu/menuApi.ts
@@ -1,34 +1,34 @@
 export class MenuApi {
+  isOrdered: boolean;
+  list: HTMLUListElement;
 
-    isOrdered: boolean;
-    list: HTMLUListElement
+  constructor(isOrdered: boolean) {
+    this.isOrdered = isOrdered;
+    this.isOrdered
+      ? (this.list = document.createElement('ul'))
+      : (this.list = document.createElement('ol'));
 
-    constructor(isOrdered: boolean) {
-        this.isOrdered = isOrdered;
-    }
+    return this;
+  }
+  addItem(text: string) {
+    const item: HTMLElement = document.createElement('li');
+    item.innerText = text;
+    this.list.appendChild(item);
+  }
 
-    create() {
-        this.isOrdered ? 
-        this.list = document.createElement("ul") : 
-        this.list = document.createElement("ol");
-    }
+  show(): void {
+    document.addEventListener('click', (e) => {
+      console.log('Test');
+      e.preventDefault();
+      let { display, position, right, bottom } = this.list.style;
+      display = 'block';
+      position = 'fixed';
+      right = '100';
+      bottom = '100';
+    });
+  }
 
-    addItem(text: string) {
-        const item:HTMLElement = document.createElement("li");
-        item.innerText = text
-        this.list.appendChild(item);
-    }
-
-    show() {
-        let {display, position, right, bottom} = this.list.style;
-        display = "block";
-        position = "fixed";
-        right = "100";
-        bottom = "100";
-    }
-
-    hide() {
-        this.list.style.display = "none";
-    }
-
+  hide() {
+    this.list.style.display = 'none';
+  }
 }
