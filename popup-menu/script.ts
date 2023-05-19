bf216d4d88962566d8ba004a78453da7084df608
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-05-19 15:08:39 +0200

    Add first menu instance

--- popup-menu/script.ts
+++ popup-menu/script.ts
@@ -1,3 +1,14 @@
 import { MenuApi as Menu } from './menuApi.js';
-const menu = new Menu(true);
-console.log(menu);
+
+const menu = new Menu('grey');
+
+menu.addItems('Menu List', 'Item-2', 'Item-3', 'Item-2', 'Item-3');
+
+document.addEventListener('contextmenu', (e) => {
+  e.preventDefault();
+  if (!menu.is_displayed) {
+    menu.show(e);
+  } else {
+    menu.hide();
+  }
+});
