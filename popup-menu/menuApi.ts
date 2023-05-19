6003f5e6b2442e2e96bb26e7a3e0400f2020b917
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-05-19 15:08:26 +0200

    Update menuApi

--- popup-menu/menuApi.ts
+++ popup-menu/menuApi.ts
@@ -1,34 +1,71 @@
 export class MenuApi {
-  isOrdered: boolean;
-  list: HTMLUListElement;
+  list: HTMLElement;
+  is_displayed: boolean;
+  display_type: string;
+  is_moving: boolean;
 
-  constructor(isOrdered: boolean) {
-    this.isOrdered = isOrdered;
-    this.isOrdered
-      ? (this.list = document.createElement('ul'))
-      : (this.list = document.createElement('ol'));
+  constructor(
+    bgcolor: string = 'grey',
+    display: string = 'flex',
+    padding: string = '20px',
+    align: string = 'left'
+  ) {
+    // create new list element
+    this.list = document.createElement('ul');
+    this.is_displayed = false;
+    this.is_moving = false;
+    this.display_type = display;
 
+    // list base-style-configuration
+    this.list.style.display = 'none';
+    this.list.style.position = 'absolute';
+    this.list.style.left = '0px';
+    this.list.style.top = '0px';
+    this.list.style.flexDirection = 'column';
+    this.list.style.alignItems = 'center';
+    this.list.style.justifyContent = 'center';
+    this.list.style.height = 'auto';
+    this.list.style.width = 'auto';
+    // optional styling
+    this.list.style.backgroundColor = bgcolor;
+    this.list.style.textAlign = align;
+    this.list.style.padding = padding;
+
+    // append to root div
+    document.getElementById('display')?.appendChild(this.list);
     return this;
   }
-  addItem(text: string) {
-    const item: HTMLElement = document.createElement('li');
-    item.innerText = text;
-    this.list.appendChild(item);
-  }
 
-  show(): void {
-    document.addEventListener('click', (e) => {
-      console.log('Test');
-      e.preventDefault();
-      let { display, position, right, bottom } = this.list.style;
-      display = 'block';
-      position = 'fixed';
-      right = '100';
-      bottom = '100';
+  addItems(title: string, ...inputs: string[]) {
+    const items = [...inputs];
+
+    const headline: HTMLElement = document.createElement('h4');
+    headline.innerText = title;
+    this.list.appendChild(headline);
+
+    items.forEach((el) => {
+      const item: HTMLElement = document.createElement('li');
+      item.innerText = el;
+      item.style.listStyle = 'none';
+      item.style.margin = '5px 0px';
+      this.list.appendChild(item);
     });
   }
 
-  hide() {
+  show(event: MouseEvent): void {
+    this.is_displayed = true;
+    this.list.style.display = this.display_type;
+    let { clientX: x, clientY: y } = event;
+    this.list.style.transform = `translate(${x}px, ${y}px)`;
+  }
+
+  hide(): void {
+    this.is_displayed = false;
     this.list.style.display = 'none';
+  }
+
+  move(event: MouseEvent): void {
+    let { clientX: x, clientY: y } = event;
+    this.list.style.transform = `translate(${x}px, ${y}px)`;
   }
 }
