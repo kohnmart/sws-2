5b922fd5f63616385e4d85ae4f809779917a9c0f
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-05-19 15:08:48 +0200

    update js-dist

--- popup-menu/dist/menuApi.js
+++ popup-menu/dist/menuApi.js
@@ -1,28 +1,59 @@
 export class MenuApi {
-    constructor(isOrdered) {
-        this.isOrdered = isOrdered;
-        this.isOrdered
-            ? (this.list = document.createElement('ul'))
-            : (this.list = document.createElement('ol'));
+    constructor(bgcolor = 'grey', display = 'flex', padding = '20px', align = 'left') {
+        var _a;
+        // create new list element
+        this.list = document.createElement('ul');
+        this.is_displayed = false;
+        this.is_moving = false;
+        this.display_type = display;
+        // list base-style-configuration
+        this.list.style.display = 'none';
+        this.list.style.position = 'absolute';
+        this.list.style.left = '0px';
+        this.list.style.top = '0px';
+        this.list.style.flexDirection = 'column';
+        this.list.style.alignItems = 'center';
+        this.list.style.justifyContent = 'center';
+        this.list.style.height = 'auto';
+        this.list.style.width = 'auto';
+        // optional styling
+        this.list.style.backgroundColor = bgcolor;
+        this.list.style.textAlign = align;
+        this.list.style.padding = padding;
+        // add event listening
+        this.list.addEventListener('mousedown', (e) => {
+            e.preventDefault();
+            this.move(e);
+        });
+        // append to root div
+        (_a = document.getElementById('display')) === null || _a === void 0 ? void 0 : _a.appendChild(this.list);
         return this;
     }
-    addItem(text) {
-        const item = document.createElement('li');
-        item.innerText = text;
-        this.list.appendChild(item);
-    }
-    show() {
-        document.addEventListener('click', (e) => {
-            console.log('Test');
-            e.preventDefault();
-            let { display, position, right, bottom } = this.list.style;
-            display = 'block';
-            position = 'fixed';
-            right = '100';
-            bottom = '100';
+    addItems(title, ...inputs) {
+        const items = [...inputs];
+        const headline = document.createElement('h4');
+        headline.innerText = title;
+        this.list.appendChild(headline);
+        items.forEach((el) => {
+            const item = document.createElement('li');
+            item.innerText = el;
+            item.style.listStyle = 'none';
+            item.style.margin = '5px 0px';
+            this.list.appendChild(item);
         });
     }
+    show(event) {
+        this.is_displayed = true;
+        this.list.style.display = this.display_type;
+        let { clientX: x, clientY: y } = event;
+        this.list.style.transform = `translate(${x}px, ${y}px)`;
+    }
     hide() {
+        this.is_displayed = false;
         this.list.style.display = 'none';
+    }
+    move(event) {
+        let { clientX: x, clientY: y } = event;
+        this.list.style.transform = `translate(${x}px, ${y}px)`;
     }
 }
--- popup-menu/dist/script.js
+++ popup-menu/dist/script.js
@@ -1,4 +1,12 @@
 import { MenuApi as Menu } from './menuApi.js';
-const menu = new Menu(true);
-console.log(menu);
-console.log('TEST');
+const menu = new Menu('grey');
+menu.addItems('Menu List', 'Item-2', 'Item-3', 'Item-2', 'Item-3');
+document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    if (!menu.is_displayed) {
+        menu.show(e);
+    }
+    else {
+        menu.hide();
+    }
+});
