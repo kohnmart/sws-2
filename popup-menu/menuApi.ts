5f160720301426fe737079c4782842b0cef116ae
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-05-21 12:18:41 +0200

    Add commenting

--- popup-menu/menuApi.ts
+++ popup-menu/menuApi.ts
@@ -1,27 +1,16 @@
+import { Item } from './item.js';
+
+/* ul-list element */
 let list: HTMLElement;
 
+/* create new menu and append functionality */
 const createMenu = (): any => {
   list = document.createElement('ul');
-  // list base-style-configuration
-  list.style.display = 'none';
-  list.style.position = 'absolute';
-  list.style.left = '0px';
-  list.style.top = '0px';
-  list.style.flexDirection = 'column';
-  list.style.alignItems = 'center';
-  list.style.justifyContent = 'center';
-  list.style.height = 'auto';
-  list.style.width = 'auto';
-  // optional styling
-  list.style.backgroundColor = 'grey';
-  list.style.textAlign = 'center';
-  list.style.padding = '10px';
-
-  // append to root div
   document.getElementById('display')?.appendChild(list);
   return { list, addItem, addItemAt, removeItem, show };
 };
 
+/* create new item with callback */
 const createItem = (
   item_content: string,
   _callback: (item: Item) => void
@@ -33,12 +22,14 @@ const createItem = (
   return new Item(element);
 };
 
+/* append new items to list */
 const addItem = (...items: Item[]) => {
   items.forEach((item) => {
     list.appendChild(item.element);
   });
 };
 
+/* add new item at index */
 const addItemAt = (item: Item, targetIndex: number) => {
   // Ref: https://www.w3schools.com/JSREF/met_node_replacechild.asp
   const element = list.children[targetIndex];
@@ -46,38 +37,28 @@ const addItemAt = (item: Item, targetIndex: number) => {
   list.appendChild(element);
 };
 
+/* remove item */
 const removeItem = (item: Item) => {
   list.removeChild(item.element);
 };
 
+/* create separator hr-line */
 const createSeparator = (): Item => {
   const separator: HTMLElement = document.createElement('hr');
   return new Item(separator);
 };
 
+/* display menu instance */
 const show = (x: Number, y: Number): void => {
+  // Ref: https://www.w3schools.com/JSREF/canvas_translate.asp
   list.style.display = 'block';
   list.style.transform = `translate(${x}px, ${y}px)`;
 };
 
+/* hide menu instance */
 const hide = (): void => {
   list.style.display = 'none';
 };
-
-class Item {
-  element: HTMLElement;
-  constructor(element: HTMLElement) {
-    this.element = element;
-  }
-
-  hide(): void {
-    this.element.style.display = 'none';
-  }
-
-  show(): void {
-    this.element.style.display = 'block';
-  }
-}
 
 export default {
   createMenu,
