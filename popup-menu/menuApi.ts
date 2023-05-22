6eebf50ad77e899d77778ec8adce8b78abc14f57
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-05-22 21:10:46 +0200

    Update menuApi

--- popup-menu/menuApi.ts
+++ popup-menu/menuApi.ts
@@ -1,37 +1,58 @@
-import Menu, { Item } from './menu.js';
+import { Item } from './item.js';
 
-interface IMenuApi {
-  createMenu(): Menu;
-  createItem(item_content: string, callback: (item: Item) => void): Item;
-  createSeparator(): Item;
+export default class MenuApi {
+  list: HTMLUListElement = document.createElement('ul');
+
+  /* create new menu and append functionality */
+  createMenu = () => {
+    document.getElementById('display')?.appendChild(this.list);
+    return this;
+  };
+
+  /* create new item with callback */
+  createItem = (item_content: string, callback: (m: MenuApi) => void) => {
+    return new Item('button', this, item_content, (m) => callback(m));
+  };
+
+  /* create separator hr-line */
+  createSeparator = (): Item => {
+    return new Item('hr', this);
+  };
+
+  /* add single item to list */
+  addItem = (item: Item) => {
+    this.list.appendChild(item.element);
+  };
+
+  /* append new items to list */
+  addItems = (...items: Item[]) => {
+    items.forEach((item) => {
+      this.list.appendChild(item.element);
+    });
+  };
+
+  /* add new item at index */
+  addItemAt = (item: Item, targetIndex: number) => {
+    // Ref: https://www.w3schools.com/JSREF/met_node_replacechild.asp
+    const element = this.list.children[targetIndex];
+    this.list.replaceChild(item.element, this.list.children[targetIndex]);
+    this.list.appendChild(element);
+  };
+
+  /* remove item */
+  removeItem = (item: Item) => {
+    this.list.removeChild(item.element);
+  };
+
+  /* display menu instance */
+  show = (x: Number, y: Number): void => {
+    // Ref: https://www.w3schools.com/JSREF/canvas_translate.asp
+    this.list.style.display = 'block';
+    this.list.style.transform = `translate(${x}px, ${y}px)`;
+  };
+
+  /* hide menu instance */
+  hide = (): void => {
+    this.list.style.display = 'none';
+  };
 }
-
-/* create new menu and append functionality */
-const createMenu = (): Menu => {
-  const menu = new Menu();
-  document.getElementById('display')?.appendChild(menu.list);
-  return menu;
-};
-
-/* create new item with callback */
-const createItem = (item_content: string, callback: Function) => {
-  const element = document.createElement('li');
-  element.innerText = item_content;
-  const item = new Item(element);
-  callback();
-  return item;
-};
-
-/* create separator hr-line */
-const createSeparator = (): Item => {
-  const separator: HTMLElement = document.createElement('hr');
-  return new Item(separator);
-};
-
-export default {
-  createMenu,
-  createItem,
-  createSeparator,
-};
-
-export { IMenuApi };
