5a3dc5f03bf3398967936aa55b4307587abe348e
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-05-25 20:19:54 +0200

    Comment script

--- mouse-coordinates/script.ts
+++ mouse-coordinates/script.ts
@@ -1,4 +1,4 @@
-//define render-View
+/* Create render-view */
 const width: Number = 500,
   height: Number = 500;
 const render: HTMLElement = document.getElementById('render_view')!;
@@ -6,13 +6,18 @@ render.style.width = width + 'px';
 render.style.height = height + 'px';
 render.style.backgroundColor = 'grey';
 
-//define coordinates
+/* Get span-element to display real-time mouse-coordinates */
 const coordinates: HTMLElement = document.getElementById(
   'coordinates_rt_display'
 )!;
+
+/* Create list-element to store mouse-coordinates on event */
 const list: HTMLElement = document.getElementById('coordinates_list')!;
+
+/* Store dynamic mouse-coordinates */
 let mouseX: Number, mouseY: Number;
 
+/* Display mouse-coordinates if cursor is in given range */
 document.addEventListener('mousemove', (e) => {
   ({ clientX: mouseX, clientY: mouseY } = e);
 
@@ -24,6 +29,7 @@ document.addEventListener('mousemove', (e) => {
   }
 });
 
+/* Create new list element with current mouse-coordinates */
 document.addEventListener('click', () => {
   const item: HTMLElement = document.createElement('li');
   item.innerHTML = `(MouseX: ${mouseX} || MouseY: ${mouseY})`;
