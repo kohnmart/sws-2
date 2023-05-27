8eb78a499b59a587e504d81f098129b9f6e98bde
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-05-27 20:16:45 +0200

    Update script

--- mouse-coordinates/script.ts
+++ mouse-coordinates/script.ts
@@ -1,23 +1,23 @@
-/* Create render-view */
-const width: Number = 500,
-  height: Number = 500;
-const render: HTMLElement = document.getElementById('render_view')!;
+/* create render-view */
+const width: Number = 500;
+const height: Number = 500;
+const render: HTMLElement = document.getElementById('render-view')!;
 render.style.width = width + 'px';
 render.style.height = height + 'px';
 render.style.backgroundColor = 'grey';
 
-/* Get span-element to display real-time mouse-coordinates */
+/* get span-element to display real-time mouse-coordinates */
 const coordinates: HTMLElement = document.getElementById(
-  'coordinates_rt_display'
+  'coordinates-rt-display'
 )!;
 
-/* Create list-element to store mouse-coordinates on event */
-const list: HTMLElement = document.getElementById('coordinates_list')!;
+/* create list-element to store mouse-coordinates on event */
+const list: HTMLElement = document.getElementById('coordinates-list')!;
 
-/* Store dynamic mouse-coordinates */
+/* store dynamic mouse-coordinates */
 let mouseX: Number, mouseY: Number;
 
-/* Display mouse-coordinates if cursor is in given range */
+/* display mouse-coordinates if cursor is in given range */
 document.addEventListener('mousemove', (e) => {
   ({ clientX: mouseX, clientY: mouseY } = e);
 
@@ -29,7 +29,7 @@ document.addEventListener('mousemove', (e) => {
   }
 });
 
-/* Create new list element with current mouse-coordinates */
+/* create new list element with current mouse-coordinates */
 document.addEventListener('click', () => {
   const item: HTMLElement = document.createElement('li');
   item.innerHTML = `(MouseX: ${mouseX} || MouseY: ${mouseY})`;
