26713d11cd7cbfb5db00eeb9b834ba4f657d0246
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-05-27 20:16:58 +0200

    Update dist

--- mouse-coordinates/dist/script.js
+++ mouse-coordinates/dist/script.js
@@ -1,25 +1,31 @@
-//define render-View
-var width = 500, height = 500;
-var render = document.getElementById('render_view');
+"use strict";
+/* Create render-view */
+const width = 500;
+const height = 500;
+const render = document.getElementById('render-view');
 render.style.width = width + 'px';
 render.style.height = height + 'px';
 render.style.backgroundColor = 'grey';
-//define coordinates
-var coordinates = document.getElementById('coordinates_rt_display');
-var list = document.getElementById('coordinates_list');
-var mouseX, mouseY;
-document.addEventListener('mousemove', function (e) {
-    (mouseX = e.clientX, mouseY = e.clientY);
+/* Get span-element to display real-time mouse-coordinates */
+const coordinates = document.getElementById('coordinates-rt-display');
+/* Create list-element to store mouse-coordinates on event */
+const list = document.getElementById('coordinates-list');
+/* Store dynamic mouse-coordinates */
+let mouseX, mouseY;
+/* Display mouse-coordinates if cursor is in given range */
+document.addEventListener('mousemove', (e) => {
+    ({ clientX: mouseX, clientY: mouseY } = e);
     if (mouseX <= width && mouseY <= height) {
         coordinates.style.display = 'block';
-        coordinates.textContent = "X: ".concat(mouseX, " || Y: ").concat(mouseY);
+        coordinates.textContent = `X: ${mouseX} || Y: ${mouseY}`;
     }
     else {
         coordinates.style.display = 'none';
     }
 });
-document.addEventListener('click', function () {
-    var item = document.createElement('li');
-    item.innerHTML = "(MouseX: ".concat(mouseX, " || MouseY: ").concat(mouseY, ")");
+/* Create new list element with current mouse-coordinates */
+document.addEventListener('click', () => {
+    const item = document.createElement('li');
+    item.innerHTML = `(MouseX: ${mouseX} || MouseY: ${mouseY})`;
     list.appendChild(item);
 });
