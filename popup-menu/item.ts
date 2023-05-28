f1b07d874ae4049f939d7842c6119d125a824548
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-05-28 11:40:25 +0200

    Update item

--- popup-menu/item.ts
+++ popup-menu/item.ts
@@ -1,29 +1,27 @@
 import MenuApi from './menuApi';
 
 export class Item {
-  /* ul list - element */
   element: HTMLElement;
-
-  /* menu instance to ref on callback event */
   menuInstance: MenuApi;
 
   /* create new item with callback */
   constructor(
     tagName: string,
     menuInstance: MenuApi,
-    item_content?: string,
+    itemContent?: string,
     callback?: (m: MenuApi) => void
   ) {
     this.element = document.createElement(tagName);
     this.menuInstance = menuInstance;
-    if (item_content) {
-      this.element.innerText = item_content;
+    if (itemContent) {
+      this.element.innerText = itemContent;
     }
     if (callback) {
       this.element.addEventListener('click', () => callback(this.menuInstance));
     }
   }
 
+  /* render item in ul-list */
   render(): void {
     const li = document.createElement('li');
     li.appendChild(this.element);
