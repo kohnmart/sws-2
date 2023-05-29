fe164af6709328d70e87347ac5fcbc4bad414c5b
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-05-29 10:58:53 +0200

    Update dist

--- mouse-coordinates/dist/script.js
+++ mouse-coordinates/dist/script.js
@@ -1,31 +1,31 @@
 "use strict";
-/* Create render-view */
+/* create render-view */
 const width = 500;
 const height = 500;
-const render = document.getElementById('render-view');
-render.style.width = width + 'px';
-render.style.height = height + 'px';
-render.style.backgroundColor = 'grey';
-/* Get span-element to display real-time mouse-coordinates */
+const renderView = document.getElementById('render-view');
+renderView.style.width = width + 'px';
+renderView.style.height = height + 'px';
+renderView.style.backgroundColor = 'grey';
+/* store dynamic mouse-coordinates */
+let mX, mY;
+/* get span-element to display real-time mouse-coordinates */
 const coordinates = document.getElementById('coordinates-rt-display');
-/* Create list-element to store mouse-coordinates on event */
-const list = document.getElementById('coordinates-list');
-/* Store dynamic mouse-coordinates */
-let mouseX, mouseY;
-/* Display mouse-coordinates if cursor is in given range */
+/* display mouse-coordinates if cursor is in given range */
 document.addEventListener('mousemove', (e) => {
-    ({ clientX: mouseX, clientY: mouseY } = e);
-    if (mouseX <= width && mouseY <= height) {
+    ({ clientX: mX, clientY: mY } = e);
+    if (mX <= width && mY <= height) {
         coordinates.style.display = 'block';
-        coordinates.textContent = `X: ${mouseX} || Y: ${mouseY}`;
+        coordinates.textContent = `X: ${mX} || Y: ${mY}`;
     }
     else {
         coordinates.style.display = 'none';
     }
 });
-/* Create new list element with current mouse-coordinates */
+/* create list-element to store mouse-coordinates */
+const list = document.getElementById('coordinates-list');
+/* create new list element with current mouse-coordinates */
 document.addEventListener('click', () => {
     const item = document.createElement('li');
-    item.innerHTML = `(MouseX: ${mouseX} || MouseY: ${mouseY})`;
+    item.innerHTML = `(X: ${mX} || Y: ${mY})`;
     list.appendChild(item);
 });
