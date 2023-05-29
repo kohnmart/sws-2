3784393e1e4d276f672593904ed969334ac18cc7
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-05-29 10:58:39 +0200

    Refactor script

--- mouse-coordinates/script.ts
+++ mouse-coordinates/script.ts
@@ -1,37 +1,37 @@
 /* create render-view */
 const width: Number = 500;
 const height: Number = 500;
-const render: HTMLElement = document.getElementById('render-view')!;
-render.style.width = width + 'px';
-render.style.height = height + 'px';
-render.style.backgroundColor = 'grey';
+const renderView: HTMLElement = document.getElementById('render-view')!;
+renderView.style.width = width + 'px';
+renderView.style.height = height + 'px';
+renderView.style.backgroundColor = 'grey';
+
+/* store dynamic mouse-coordinates */
+let mX: Number, mY: Number;
 
 /* get span-element to display real-time mouse-coordinates */
 const coordinates: HTMLElement = document.getElementById(
   'coordinates-rt-display'
 )!;
 
-/* create list-element to store mouse-coordinates on event */
-const list: HTMLElement = document.getElementById('coordinates-list')!;
-
-/* store dynamic mouse-coordinates */
-let mouseX: Number, mouseY: Number;
-
 /* display mouse-coordinates if cursor is in given range */
 document.addEventListener('mousemove', (e) => {
-  ({ clientX: mouseX, clientY: mouseY } = e);
+  ({ clientX: mX, clientY: mY } = e);
 
-  if (mouseX <= width && mouseY <= height) {
+  if (mX <= width && mY <= height) {
     coordinates.style.display = 'block';
-    coordinates.textContent = `X: ${mouseX} || Y: ${mouseY}`;
+    coordinates.textContent = `X: ${mX} || Y: ${mY}`;
   } else {
     coordinates.style.display = 'none';
   }
 });
 
+/* create list-element to store mouse-coordinates */
+const list: HTMLElement = document.getElementById('coordinates-list')!;
+
 /* create new list element with current mouse-coordinates */
 document.addEventListener('click', () => {
   const item: HTMLElement = document.createElement('li');
-  item.innerHTML = `(MouseX: ${mouseX} || MouseY: ${mouseY})`;
+  item.innerHTML = `(X: ${mX} || Y: ${mY})`;
   list.appendChild(item);
 });
