681268aa82051114f77eae1ae0219ee11ca0a535
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-06-08 11:32:43 +0200

    Update selector all shapes in

--- drawer-einfach/Selector.ts
+++ drawer-einfach/Selector.ts
@@ -8,23 +8,35 @@ export class Selector {
   public static iterateShapes(x: number, y: number) {
     const ctx = Selector.canvas.getCanvasRenderingContext();
     const shapes = Selector.canvas.getShapes();
+    /* Iterate over shapes */
     for (const key in shapes) {
       if (shapes.hasOwnProperty(key)) {
         const shape = shapes[key];
         const type = shape.type;
 
+        /* Check the type */
         if (type == 'line') {
           const line = shape as Line;
           const { x: start_x, y: start_y } = line.from;
           const { x: end_x, y: end_y } = line.to;
-          const distance =
-            ((end_y - start_y) * x -
-              (end_x - start_x) * y +
-              end_x * start_y -
-              end_y * start_x) /
-            ((end_y - start_y) ** 2 + (end_x - start_x) ** 2) ** 0.5;
 
-          Selector.checkThreshold(distance, 10, line, ctx);
+          // Calculate the cross product between vectors (start to point) and (start to end)
+          const crossProduct =
+            (x - start_x) * (end_y - start_y) -
+            (y - start_y) * (end_x - start_x);
+
+          // Check if the point is collinear with the line segment
+          if (Math.abs(crossProduct) < 1500) {
+            // Check if the point lies within the bounding box of the line segment
+            if (
+              Math.min(start_x, end_x) <= x &&
+              x <= Math.max(start_x, end_x) &&
+              Math.min(start_y, end_y) <= y &&
+              y <= Math.max(start_y, end_y)
+            ) {
+              shape.draw(ctx, true);
+            }
+          }
         } else if (type == 'rectangle') {
           const rectangle = shape as Rectangle;
           const { x: start_x, y: start_y } = rectangle.from;
@@ -36,45 +48,42 @@ export class Selector {
           const distanceToTop = Math.abs(y - start_y);
           const distanceToBottom = Math.abs(y - end_y);
 
-          console.log('--------------------');
-          console.log('Distance to Left: ' + distanceToLeft);
-          console.log('Distance to Right: ' + distanceToRight);
-          console.log('Distance to Top: ' + distanceToTop);
-          console.log('Distance to Bottom: ' + distanceToBottom);
-
-          console.log('Dist: ' + Math.abs(end_x - start_x));
-          console.log('Dist: ' + Math.abs(end_y - start_y));
-
           // Check if the point is within the rectangle's boundaries
           if (
-            distanceToLeft <= Math.abs(end_x - start_x) && // Check if the point is to the right of the left side
-            distanceToRight <= Math.abs(end_x - start_x) && // Check if the point is to the left of the right side
-            distanceToTop <= end_y - start_y && // Check if the point is below the top side
-            distanceToBottom <= end_y - start_y // Check if the point is above the bottom side
+            distanceToLeft <= Math.abs(end_x - start_x) &&
+            distanceToRight <= Math.abs(end_x - start_x) &&
+            distanceToTop <= end_y - start_y &&
+            distanceToBottom <= end_y - start_y
           ) {
-            ('DRAW');
             rectangle.draw(ctx, true);
           }
         } else if (type == 'triangle') {
-          const rectangle = shape as Triangle;
-          const { p1, p2, p3 } = rectangle;
+          const triangle = shape as Triangle;
+          const { p1, p2, p3 } = triangle;
+
+          // Calculate barycentric coordinates of the point with respect to the triangle
+          const alpha =
+            ((p2.y - p3.y) * (x - p3.x) + (p3.x - p2.x) * (y - p3.y)) /
+            ((p2.y - p3.y) * (p1.x - p3.x) + (p3.x - p2.x) * (p1.y - p3.y));
+          const beta =
+            ((p3.y - p1.y) * (x - p3.x) + (p1.x - p3.x) * (y - p3.y)) /
+            ((p2.y - p3.y) * (p1.x - p3.x) + (p3.x - p2.x) * (p1.y - p3.y));
+          const gamma = 1 - alpha - beta;
+
+          if (alpha >= 0 && beta >= 0 && gamma >= 0) {
+            triangle.draw(ctx, true);
+          }
         } else {
           const circle = shape as Circle;
           const { radius, center } = circle;
+          // Calculate the distance between the point and the center of the circle
+          const distance = Math.sqrt((x - center.x) ** 2 + (y - center.y) ** 2);
+
+          if (distance <= radius) {
+            circle.draw(ctx, true);
+          }
         }
       }
-    }
-  }
-
-  static checkThreshold(
-    distance: number,
-    thrs: number,
-    shape: Line | Circle | Rectangle | Triangle,
-    ctx: CanvasRenderingContext2D
-  ) {
-    console.log(distance);
-    if (Math.abs(distance) <= thrs) {
-      shape.draw(ctx, true);
     }
   }
 }
