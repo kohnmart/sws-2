48a7bb177164bf6f8f4b6d646bc600133ed60705
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-06-11 15:15:27 +0200

    Update item extension

--- drawer-einfach/item.ts
+++ drawer-einfach/item.ts
@@ -1,9 +1,9 @@
 import MenuApi from './menuApi';
 
 export class Item {
-  static id: string = 'menu-item';
-  element: HTMLElement;
-  menuInstance: MenuApi;
+  public static id: string = 'menu-item';
+  public container: HTMLElement;
+  private menuInstance: MenuApi;
   /* create new item */
   constructor(
     tagName: string,
@@ -11,21 +11,61 @@ export class Item {
     itemContent?: string,
     callback?: (m: MenuApi) => void
   ) {
-    this.element = document.createElement(tagName);
-    this.element.id = Item.id;
+    this.container = document.createElement(tagName);
+    this.container.id = Item.id;
     this.menuInstance = menuInstance;
     if (itemContent) {
-      this.element.innerText = itemContent;
+      this.container.innerText = itemContent;
     }
     if (callback) {
-      this.element.addEventListener('click', () => callback(this.menuInstance));
+      this.container.addEventListener('click', () =>
+        callback(this.menuInstance)
+      );
     }
   }
 
   /* render item in ul-list */
   render(): void {
     const li = document.createElement('li');
-    li.appendChild(this.element);
+    li.appendChild(this.container);
     this.menuInstance.ulList.appendChild(li);
+  }
+}
+
+export class ItemColor extends Item {
+  key: string;
+  value: string;
+  defaultColor: string;
+  container: HTMLElement;
+  inputElement: HTMLInputElement;
+  labelElement: HTMLLabelElement;
+
+  constructor(
+    tagName: string,
+    menuInstance: MenuApi,
+    key: string,
+    value: string,
+    selectedColor: string,
+    callback?: (m: MenuApi) => void
+  ) {
+    super(tagName, menuInstance, null, callback);
+    this.key = key;
+    this.defaultColor = selectedColor;
+    this.inputElement = document.createElement('input');
+    this.inputElement.type = 'radio';
+    this.inputElement.name = key;
+    this.inputElement.value = value;
+
+    this.labelElement = document.createElement('label');
+    this.labelElement.textContent = value;
+    this.labelElement.htmlFor = value;
+
+    this.container.append(this.inputElement);
+    this.container.append(this.labelElement);
+  }
+
+  setColorOption(color: string): void {
+    this.defaultColor = color;
+    // Additional logic related to color setting
   }
 }
