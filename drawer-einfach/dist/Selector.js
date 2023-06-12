bc7304d7f5d85e7f5070069651da849f654ff227
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-06-12 21:20:24 +0200

    Update dist

--- drawer-einfach/dist/Selector.js
+++ drawer-einfach/dist/Selector.js
@@ -24,7 +24,6 @@ export class Selector {
     handleMouseMove() {
         return;
     }
-    /* Scanning shapes */
     static iterateShapes(x, y, isCtrl) {
         const ctx = Selector.canvas.getCanvasRenderingContext();
         const shapes = Selector.canvas.getShapes();
@@ -37,74 +36,96 @@ export class Selector {
             if (shapes.hasOwnProperty(key)) {
                 const shape = shapes[key];
                 const type = shape.type;
-                /* Check the type */
-                if (type == 'line') {
-                    const line = shape;
-                    const { x: start_x, y: start_y } = line.from;
-                    const { x: end_x, y: end_y } = line.to;
-                    // Calculate the cross product between vectors (start to point) and (start to end)
-                    const crossProduct = (x - start_x) * (end_y - start_y) -
-                        (y - start_y) * (end_x - start_x);
-                    // Check if the point is collinear with the line segment
-                    if (Math.abs(crossProduct) < 1500) {
-                        if (Math.min(start_x, end_x) <= x &&
-                            x <= Math.max(start_x, end_x) &&
-                            Math.min(start_y, end_y) <= y &&
-                            y <= Math.max(start_y, end_y)) {
+                switch (type) {
+                    case 'line':
+                        const line = shape;
+                        const { from, to } = line;
+                        if (Selector.checkLineIntersection(x, y, from, to)) {
                             Selector.shapeListId.push(line.id);
                         }
-                    }
-                }
-                else if (type == 'rectangle') {
-                    const rectangle = shape;
-                    const { x: start_x, y: start_y } = rectangle.from;
-                    const { x: end_x, y: end_y } = rectangle.to;
-                    // Calculate the distances from the point to each side of the rectangle
-                    const distanceToLeft = Math.abs(x - start_x);
-                    const distanceToRight = Math.abs(x - end_x);
-                    const distanceToTop = Math.abs(y - start_y);
-                    const distanceToBottom = Math.abs(y - end_y);
-                    // Check if the point is within the rectangle's boundaries
-                    if (distanceToLeft <= Math.abs(end_x - start_x) &&
-                        distanceToRight <= Math.abs(end_x - start_x) &&
-                        distanceToTop <= end_y - start_y &&
-                        distanceToBottom <= end_y - start_y) {
-                        Selector.shapeListId.push(rectangle.id);
-                    }
-                }
-                else if (type == 'triangle') {
-                    const triangle = shape;
-                    const { p1, p2, p3 } = triangle;
-                    // Calculate barycentric coordinates of the point with respect to the triangle
-                    const alpha = ((p2.y - p3.y) * (x - p3.x) + (p3.x - p2.x) * (y - p3.y)) /
-                        ((p2.y - p3.y) * (p1.x - p3.x) + (p3.x - p2.x) * (p1.y - p3.y));
-                    const beta = ((p3.y - p1.y) * (x - p3.x) + (p1.x - p3.x) * (y - p3.y)) /
-                        ((p2.y - p3.y) * (p1.x - p3.x) + (p3.x - p2.x) * (p1.y - p3.y));
-                    const gamma = 1 - alpha - beta;
-                    if (alpha >= 0 && beta >= 0 && gamma >= 0) {
-                        Selector.shapeListId.push(triangle.id);
-                    }
-                }
-                else {
-                    const circle = shape;
-                    const { radius, center } = circle;
-                    // Calculate the distance between the point and the center of the circle
-                    const distance = Math.sqrt(Math.pow((x - center.x), 2) + Math.pow((y - center.y), 2));
-                    if (distance <= radius) {
-                        Selector.shapeListId.push(circle.id);
-                    }
+                        break;
+                    case 'rectangle':
+                        const rectangle = shape;
+                        const { from: rectFrom, to: rectTo } = rectangle;
+                        if (Selector.checkPointInRectangle(x, y, rectFrom, rectTo)) {
+                            Selector.shapeListId.push(rectangle.id);
+                        }
+                        break;
+                    case 'triangle':
+                        const triangle = shape;
+                        const { p1, p2, p3 } = triangle;
+                        if (Selector.checkPointInTriangle(x, y, p1, p2, p3)) {
+                            Selector.shapeListId.push(triangle.id);
+                        }
+                        break;
+                    case 'circle':
+                        const circle = shape;
+                        const { center, radius } = circle;
+                        if (Selector.checkPointInCircle(x, y, center, radius)) {
+                            Selector.shapeListId.push(circle.id);
+                        }
+                        break;
                 }
             }
         }
-        if (Selector.shapeListId.length && !isCtrl) {
-            const id = Selector.shapeListId[0];
-            shapes[id].draw(ctx, true);
-        }
-        else if (Selector.shapeListId.length && isCtrl) {
-            Selector.shapeListId.forEach((id) => {
+        if (Selector.shapeListId.length) {
+            if (!isCtrl) {
+                const id = Selector.shapeListId[0];
                 shapes[id].draw(ctx, true);
-            });
+            }
+            else {
+                Selector.shapeListId.forEach((id) => {
+                    shapes[id].draw(ctx, true);
+                });
+            }
         }
+    }
+    static checkLineIntersection(x, y, from, to) {
+        const { x: start_x, y: start_y } = from;
+        const { x: end_x, y: end_y } = to;
+        const crossProduct = (x - start_x) * (end_y - start_y) - (y - start_y) * (end_x - start_x);
+        if (Math.abs(crossProduct) < 1500) {
+            if (Math.min(start_x, end_x) <= x &&
+                x <= Math.max(start_x, end_x) &&
+                Math.min(start_y, end_y) <= y &&
+                y <= Math.max(start_y, end_y)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    static checkPointInRectangle(x, y, from, to) {
+        const { x: start_x, y: start_y } = from;
+        const { x: end_x, y: end_y } = to;
+        const distanceToLeft = Math.abs(x - start_x);
+        const distanceToRight = Math.abs(x - end_x);
+        const distanceToTop = Math.abs(y - start_y);
+        const distanceToBottom = Math.abs(y - end_y);
+        if (distanceToLeft <= Math.abs(end_x - start_x) &&
+            distanceToRight <= Math.abs(end_x - start_x) &&
+            distanceToTop <= end_y - start_y &&
+            distanceToBottom <= end_y - start_y) {
+            return true;
+        }
+        return false;
+    }
+    static checkPointInTriangle(x, y, p1, p2, p3) {
+        const alpha = ((p2.y - p3.y) * (x - p3.x) + (p3.x - p2.x) * (y - p3.y)) /
+            ((p2.y - p3.y) * (p1.x - p3.x) + (p3.x - p2.x) * (p1.y - p3.y));
+        const beta = ((p3.y - p1.y) * (x - p3.x) + (p1.x - p3.x) * (y - p3.y)) /
+            ((p2.y - p3.y) * (p1.x - p3.x) + (p3.x - p2.x) * (p1.y - p3.y));
+        const gamma = 1 - alpha - beta;
+        if (alpha >= 0 && beta >= 0 && gamma >= 0) {
+            return true;
+        }
+        return false;
+    }
+    static checkPointInCircle(x, y, center, radius) {
+        const distance = Math.sqrt(Math.pow((x - center.x), 2) + Math.pow((y - center.y), 2));
+        if (distance <= radius) {
+            return true;
+        }
+        return false;
     }
     static handleShapesList() {
         const ctx = Selector.canvas.getCanvasRenderingContext();
