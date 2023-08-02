421f421a6de749926ef34fa7f916e5b6ab517619
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-08-02 12:16:40 +0200

    ws comcheck complete

--- app/index.ts
+++ app/index.ts
@@ -1,39 +1,26 @@
 import express from 'express';
 import http from 'http';
 import cors from 'cors';
-import { WebSocketServer } from 'ws';
+import startWebSocketServer from './websocket.js'; // Import the WebSocket server module
 
 const app = express();
 app.use(cors());
 app.use(express.static('./public'));
 
 const server = http.createServer(app);
-const wss = new WebSocketServer({ server });
 
-wss.on('connection', (ws) => {
-  console.log('WebSocket client connected');
-
-  ws.on('message', (message) => {
-    console.log('Received message:', message);
-    // Handle received message
-  });
-
-  ws.on('close', () => {
-    console.log('WebSocket client disconnected');
-    // Handle client disconnection
-  });
-});
+startWebSocketServer(server);
 
 app.get('/', function (req, res) {
-  res.sendFile('index.html', { root: './public' });
+  res.sendFile('index.html', { root: 'public' });
 });
 
-app.get('/canvas/', function (req, res) {
-  res.sendFile('canvas.html', { root: './public' });
+app.get('/canvas/:id?', function (req, res) {
+  res.sendFile('canvas.html', { root: 'public' });
 });
 
 app.get('/overview', function (req, res) {
-  res.sendFile('overview.html', { root: './public' });
+  res.sendFile('overview.html', { root: 'public' });
 });
 
 const port = 3000;
---
+++ public/canvas/client/wsHandler.js
@@ -0,0 +1,22 @@
+// Establish WebSocket connection to the server
+const ws = new WebSocket('ws://localhost:3000/canvas');
+const wsInit = () => {
+    // When the WebSocket connection is established, receive the client ID from the server
+    ws.onopen = () => {
+        console.log('WebSocket connection established');
+    };
+    ws.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        if (data.type === 'clientId') {
+            const clientId = data.clientId;
+            console.log('Received client ID:', clientId);
+            // Do something with the client ID, e.g., use it to identify the client on the canvas page
+        }
+    };
+    ws.onclose = () => {
+        console.log('WebSocket connection closed');
+    };
+};
+wsInit();
+export default wsInit;
+//# sourceMappingURL=wsHandler.js.map
