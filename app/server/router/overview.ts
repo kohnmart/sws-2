7d25486c81c2dfb32ef94c0cf53cbb77b2e0f49f
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-08-03 16:27:17 +0200

    Update overview routings

--- app/server/router/overview.ts
+++ app/server/router/overview.ts
@@ -1,12 +1,20 @@
 // overview.ts
 import express from 'express';
 import { checkHostExists } from '../middleware/host';
-import { addCanvasQuery } from '../db/setup';
+import { addCanvasQuery, getAllCanvasQuery } from '../db/setup';
 const indexRouter = express.Router();
 
 // Define a route for the overview page
-indexRouter.get('/', function (req, res) {
+indexRouter.get('/', checkHostExists, async (req, res) => {
   res.sendFile('index.html', { root: 'public' });
+});
+
+indexRouter.get('/all-canvas', checkHostExists, async (req, res) => {
+  const canvasList = await getAllCanvasQuery();
+  if (canvasList) {
+    res.status(200).json({ list: canvasList });
+  }
+  res.status(400).json({ message: 'Cant fetch canvas list' });
 });
 
 indexRouter.post('/create-canvas', checkHostExists, async (req, res) => {
