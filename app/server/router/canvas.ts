4956967f3449814dfb506a82b353ae4e7307ad28
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-08-18 13:45:34 +0200

    update router return obj

--- app/server/router/canvas.ts
+++ app/server/router/canvas.ts
@@ -5,13 +5,13 @@ import { canvasExists } from '../middleware/canvas.js';
 const canvasRouter = express.Router();
 
 canvasRouter.get('/:id', canvasExists, async (req, res) => {
-  res.json({ status: 200 });
+  res.json({ status: 400 });
 });
 
 canvasRouter.get('/remove/:id', canvasExists, async (req, res) => {
   try {
     await deleteCanvasQuery(req.params.id);
-    res.json({ status: 200, id: req.params.id });
+    res.json({ status: 200, content: req.params.id });
   } catch {
     console.log('Error on delete...');
   }
--- app/server/router/index.js
+++ app/server/router/index.js
@@ -5,23 +5,24 @@ const indexRouter = express.Router();
 indexRouter.get('/all-canvas', async (req, res) => {
     const canvasList = await getAllCanvasQuery();
     if (canvasList) {
-        res.json({ status: 200, list: canvasList });
+        res.json({ status: 200, content: canvasList });
     }
     else {
-        res.json({ status: 400, message: 'Cannot fetch canvas list' });
+        res.json({ status: 400, content: 'Cannot fetch canvas list' });
     }
 });
 indexRouter.post('/create', checkHostExists, async (req, res) => {
     const canvasId = await addCanvasQuery(req.body.hostId, req.body.name);
     if (canvasId) {
-        return res.status(200).json({
-            msg: {
+        return res.json({
+            status: 200,
+            content: {
                 name: req.body.name,
                 hostId: req.body.hostId,
                 canvasId: canvasId,
             },
         });
     }
-    return res.status(400).json({ msg: 'error on canvas creation' });
+    return res.json({ status: 400, content: 'error on canvas creation' });
 });
 export default indexRouter;
